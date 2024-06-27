75b672bb5beaac252277a788216311de16ba33d7
HanSol Kim <yg91238405@gmail.com>
2024-06-27 12:13:07 +0900

    #2 13-1. if 조건문 #2 13-5 switch 조건문

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -96,6 +96,34 @@ namespace CSPractice1
             // 굉장히 중요
 
 
+            // #2 13-1. if 조건문
+            bool condition = true;
+            if (condition)
+            {
+                // some
+            }
+            else if (condition)
+            {
+                // some2
+            }
+            else
+            {
+                // other
+            }
+
+            int v = 0;
+            // #2 13-5 switch 조건문
+            switch (v)
+            {
+                case 0:
+                    //
+                    break;
+                default:
+                    //
+                    break;
+            }
+
+
         }
     }
 }
