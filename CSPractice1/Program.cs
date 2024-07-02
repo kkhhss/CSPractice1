36ebf91282a69ec38b59a36689af8c72924cddfa
HanSol Kim <yg91238405@gmail.com>
2024-07-02 14:48:42 +0900

    22-9 값 복사와 참조 복사

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -72,6 +72,14 @@ namespace CSPractice1
 
     internal class Program
     {
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test t)
+        {
+            t.value = 20;
+        }
 
         class ThirdClass
         {
@@ -283,7 +291,12 @@ namespace CSPractice1
             b.Height = -100;
 
             // #5 22-9 값 복사와 참조 복사
+            Test test = new Test();
+            test.value = 100;
+            Change(test);
+            Console.WriteLine(test.value); // 20
 
+            // #6 23-4 is키워드 (+as 키워드)
 
 
 
