18308c403fb9e01bd6c5fc8452868866543f87bf
HanSol Kim <yg91238405@gmail.com>
2024-07-02 15:09:17 +0900

    #6 23-6 상속의 생성자

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -288,7 +288,7 @@ namespace CSPractice1
             Box b = new Box();
             b.Width = 180;
             b.Height = 100;
-            b.Height = -100;
+            // b.Height = -100;
 
             // #5 22-9 값 복사와 참조 복사
             Test test = new Test();
@@ -317,6 +317,9 @@ namespace CSPractice1
                 }
             }
 
+            // #6 23-6 상속의 생성자
+            Child child = new Child();
+            Child child2 = new Child("홍길동");
 
 
 
